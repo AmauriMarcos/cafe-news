855f92e81f86068beb36ee107ed831222218b45d
AmauriMarcos <amaurisantos.m@gmail.com>
2020-05-12 09:30:27 +0200

    first commit

---
+++ src/components/News.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+      <h2>{{text}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+   data(){
+     return{
+       text: 'Hello'
+     }
+   }
+};
+</script>
+
+<style lang="scss">
+
+</style>
